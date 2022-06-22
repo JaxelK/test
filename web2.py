11d40083dabada8ea701879f4f73fb9efb3ff5b1
JaxelK <102785742+JaxelK@users.noreply.github.com>
2022-06-22 22:26:45 +0700

    Update web2.py

--- web2.py
+++ web2.py
@@ -3,10 +3,10 @@ import cv2
 
 st.tittle('68 LandMarks place')
 Run=st.checkbox('run')
-    FRAME_WINDOW = st.image([])
-    cam=cv2.VideoCapture(1)
-    while run:
-        ret,frame=cam.read()
-        FRAME_WINDOW.image(frame)
-    else:
-        srt.write('stopped')
+FRAME_WINDOW = st.image([])
+cam=cv2.VideoCapture(1)
+while run:
+    ret,frame=cam.read()
+    FRAME_WINDOW.image(frame)
+else:
+    srt.write('stopped')
