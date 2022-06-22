58a74ae00fbd511ad3c8c9cc5b3bd537a0344136
JaxelK <102785742+JaxelK@users.noreply.github.com>
2022-06-22 22:23:42 +0700

    Update web2.py

--- web2.py
+++ web2.py
@@ -3,7 +3,10 @@ import cv2
 
 image_placeholder = st.empty()
 if st.button('Start'):
-    video = cv2.VideoCapture('https://videos3.earthcam.com/fecnetwork/9974.flv/chunklist_w372707020.m3u8?__fvd__')
-    while True:
-        success, image = video.read()
-        image_placeholder.image(image)
+    FRAME_WINDOW = st.image([])
+    cam=cv2.VideoCapture(1)
+    while run:
+        ret,frame=cam.read()
+        FRAME_WINDOW.image(frame)
+    else:
+        srt.write('stopped')
